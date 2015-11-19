26737723714dab74e3ee48ccecf6d5fdfaa8a699
Ludovic Bonivert <ludovic.bonivert@gmail.com>
2015-11-19 19:53:12 +0100

    All in one button (play/pause)

--- app/src/main/java/com/ludovicsvetlana/xlair/MainActivity.java
+++ app/src/main/java/com/ludovicsvetlana/xlair/MainActivity.java
@@ -6,70 +6,119 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.Toast;
 
 import java.io.IOException;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements MediaController.MediaPlayerControl {
 
     MediaPlayer mPlayer;
     Button playButton;
-    Button pauseButton;
+    boolean playerIsPlaying = false;
     public String URLToStream = "http://planning.rits.be:8000/stream.mp3";
 
+    protected void createAndPlayMusicPlayer(){
+        mPlayer = new MediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mPlayer.setDataSource(URLToStream);
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+        } catch (SecurityException e) {
+            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+        } catch (IllegalStateException e) {
+            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mPlayer.prepare();
+        } catch (IllegalStateException e) {
+            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+        }
+        mPlayer.start();
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        playerIsPlaying = false;
         playButton = (Button) findViewById(R.id.playbtn);
-        pauseButton = (Button) findViewById(R.id.pausebtn);
-
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                mPlayer = new MediaPlayer();
-                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-                try {
-                    mPlayer.setDataSource(URLToStream);
-                } catch (IllegalArgumentException e) {
-                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-                } catch (SecurityException e) {
-                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-                } catch (IllegalStateException e) {
-                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    mPlayer.prepare();
-                } catch (IllegalStateException e) {
-                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-                }
-
-                mPlayer.start();
-
+                // Do we need to play or pause the player ?
+                    if (playerIsPlaying == false) {
+                        createAndPlayMusicPlayer();
+                        playButton.setText("Pause");
+                        playerIsPlaying = true;
+                    } else {
+                        mPlayer.stop();
+                        playButton.setText("Play");
+                        playerIsPlaying = false;
+                    }
             }
         });
-
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mPlayer != null  && mPlayer.isPlaying()){
-                    mPlayer.stop();
-                }
-            }
-        });
-
-
-
 
     }
+
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+
+
 }
