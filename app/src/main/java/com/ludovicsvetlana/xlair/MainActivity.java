15cbee9028108ca26549d4024c5796dbbe246aa2
Ludovic Bonivert <ludovic.bonivert@gmail.com>
2015-11-19 19:19:29 +0100

    Simple Play and pause button

--- app/src/main/java/com/ludovicsvetlana/xlair/MainActivity.java
+++ app/src/main/java/com/ludovicsvetlana/xlair/MainActivity.java
@@ -4,44 +4,71 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
 
+    MediaPlayer mPlayer;
+    Button playButton;
+    Button pauseButton;
     public String URLToStream = "http://planning.rits.be:8000/stream.mp3";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mPlayer = new MediaPlayer();
-        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        playButton = (Button) findViewById(R.id.playbtn);
+        pauseButton = (Button) findViewById(R.id.pausebtn);
 
-        try{
-            mPlayer.setDataSource(URLToStream);
-        } catch(IllegalArgumentException e){
-            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        }catch (SecurityException e) {
-            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        } catch (IllegalStateException e) {
-            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        try {
-            mPlayer.prepare();
-        }  catch (IllegalStateException e) {
-            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        }catch (IOException e){
-            Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        }
+                mPlayer = new MediaPlayer();
+                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
-       mPlayer.start();
+                try {
+                    mPlayer.setDataSource(URLToStream);
+                } catch (IllegalArgumentException e) {
+                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (SecurityException e) {
+                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    mPlayer.prepare();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                }
+
+                mPlayer.start();
+
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mPlayer != null  && mPlayer.isPlaying()){
+                    mPlayer.stop();
+                }
+            }
+        });
+
+
 
 
     }
