ebc789c609d4ceecbde80d8873084a096f16d0e5
Ludovic Bonivert <ludovic.bonivert@gmail.com>
2016-01-14 18:24:15 +0100

    Cleaned up code (deleted twitter_fragment)

--- app/src/main/java/com/ludovicsvetlana/xlair/MainActivity.java
+++ app/src/main/java/com/ludovicsvetlana/xlair/MainActivity.java
@@ -5,7 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.AppSession;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
+        // http://stackoverflow.com/questions/28541459/getting-403-forbidden-when-using-twitter-fabric-to-get-user-timeline
+        TwitterCore.getInstance().logInGuest(new Callback<AppSession>() {
+            @Override
+            public void success(Result<AppSession> result) {
+
+            }
+
+            @Override
+            public void failure(TwitterException e) {
+
+            }
+        });
+
         setContentView(R.layout.activity_main);
 
     }
--- app/src/main/java/com/ludovicsvetlana/xlair/TweetListFragment.java
+++ app/src/main/java/com/ludovicsvetlana/xlair/TweetListFragment.java
@@ -1,16 +1,13 @@
 package com.ludovicsvetlana.xlair;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
-import com.twitter.sdk.android.tweetui.UserTimeline;
 
 /**
  * Created by ludovicbonivert on 18/12/15.
@@ -21,7 +18,7 @@ public class TweetListFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.timeline, container, false);
+        View view = inflater.inflate(R.layout.twitter_timeline, container, false);
         return view;
     }
 
@@ -37,5 +34,7 @@ public class TweetListFragment extends ListFragment {
         setListAdapter(tweetAdapter);
 
     }
+
+
 
 }
